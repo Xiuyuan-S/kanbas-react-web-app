ae199272d55823b3d1ba553bc6d7dd0a0248ec84
Nicole Song <xiuyuan128@yahoo.com>
2023-10-19 21:17:06 -0400

    1111

--- src/Kanbas/Courses/Modules/index.js
+++ src/Kanbas/Courses/Modules/index.js
@@ -1,35 +1,14 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import db from "../../Database";
+import ModuleList from "./ModuleList";
+import Buttons from "./buttons";
 
-function ModuleList() {
-  const { courseId } = useParams();
-  const modules = db.modules;
-
+function Modules() {
   return (
-    <div className="list-group mt-5 col-12">
-      {modules
-        .filter((module) => module.course === courseId)
-        .map((module, index) => (
-          <div key={index} className="list-group mt-3 col-12" style={{ width: '900px' }}>
-            <a href="#" className="list-group-item list-group-item-action list-group-item-secondary">
-              {module.name}
-              <span className="icon-right">
-              </span>
-            </a>
-            <div className="collapse show">
-              {module.description.split('\n').map((desc, descIndex) => (
-                <a href="#" key={descIndex} className="list-group-item list-group-item-action with-border">
-                  <div className="content-text">{desc}</div>
-                  <span className="icon-right">
-                  </span>
-                </a>
-              ))}
-            </div>
-          </div>
-        ))}
+    <div>
+      <h2>Modules</h2>
+      
+      <Buttons/>
+      <ModuleList />
     </div>
   );
 }
-
-export default ModuleList;
+export default Modules;
